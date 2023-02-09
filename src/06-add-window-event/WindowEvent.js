36c7491883abcfa506b9c02270030ca81a8d2513
Heini Ahokas <heini.ahokas@klab-berlin.com>
2023-02-09 19:16:51 +0100

    implement toggle window event

--- src/06-add-window-event/WindowEvent.js
+++ src/06-add-window-event/WindowEvent.js
@@ -1,5 +1,16 @@
-export default function WindowEvent () {
+import { useEffect } from 'react';
+
+export default function WindowEvent() {
+  const alertOnDoubleClick = () => alert('Event fired!');
+
+  useEffect(() => {
+    window.addEventListener('dblclick', alertOnDoubleClick);
+    return () => {
+      window.removeEventListener('dblclick', alertOnDoubleClick);
+    }
+  }, []);
+  
   return (
     <h2>Window event active</h2>
-  )
+  );
 }
