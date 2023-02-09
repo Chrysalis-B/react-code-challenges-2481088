badecb29b1ceb60aa7892710a9cd39dbb7b5030a
Heini Ahokas <heini.ahokas@klab-berlin.com>
2023-02-09 16:23:58 +0100

    implement form validator

--- src/03-form-validator/FormValidator.js
+++ src/03-form-validator/FormValidator.js
@@ -1,12 +1,41 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-export default function FormValidator () {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [passwordConfirm, setPasswordConfirm] = useState('')
+export default function FormValidator() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const [displayedMessages, setDisplayedMessages] = useState([]);
+
+  const messages = {
+    required: 'Please fill in all fields',
+    email: 'Email must include exactly one @-sign',
+    passwordLength: 'Password must be at least 8 characters',
+    passwordMatch: 'Passwords must match',
+    valid: 'Form is valid'
+  }
+
+  const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x);
+  const validator = (condition, msg) => msgs => condition() ? msgs : [...msgs, msg];
+
+  const validateRequired = () => !!(email && password && passwordConfirm);
+  const validateEmail = () => email.indexOf('@') !== -1 && email.indexOf('@') === email.lastIndexOf('@');
+  const validatePasswordLength = () => password.length >= 8;
+  const validatePasswordMatch = () => password === passwordConfirm;
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const msgs = pipe(
+      validator(validateRequired, messages.required),
+      validator(validateEmail, messages.email),
+      validator(validatePasswordLength, messages.passwordLength),
+      validator(validatePasswordMatch, messages.passwordMatch),
+      msgs => msgs.length === 0 ? [messages.valid] : msgs,
+    )([])
+    setDisplayedMessages(msgs);
+  }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Sign Up!</h2>
       <label htmlFor='email'>Email</label>
       <input
@@ -24,6 +53,7 @@ export default function FormValidator () {
         onChange={e => setPasswordConfirm(e.target.value)}
       />
       <input type='submit' value='Submit' />
+      <span className='message'>{displayedMessages.join(', ')}</span>
     </form>
   )
 }
