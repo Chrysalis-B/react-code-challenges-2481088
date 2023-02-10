2c664e7811749a6972be84184b17682c96bd3094
Heini Ahokas <heini.ahokas@klab-berlin.com>
2023-02-10 14:38:35 +0100

    implement color picker

--- src/07-color-picker/ColorPicker.js
+++ src/07-color-picker/ColorPicker.js
@@ -18,14 +18,14 @@ const colors = [{
   name: 'Tart Orange'
 }]
 
-export default function ColorPicker () {
+export default function ColorPicker() {
   const [backgroundColor, setBackgroundColor] = useState('white')
 
   return (
     <div className='page' style={{ backgroundColor }}>
       {
         colors.map(color => (
-          <Color key={color.hex} hex={color.hex} name={color.name} />
+          <Color key={color.hex} hex={color.hex} name={color.name} handleClick={setBackgroundColor} />
         ))
       }
     </div>
