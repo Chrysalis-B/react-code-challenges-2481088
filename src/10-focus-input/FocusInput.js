2691c2c08577482110fc6739de35196467bb0bc8
Heini Ahokas <heini.ahokas@klab-berlin.com>
2023-02-10 17:41:01 +0100

    implement focus input

--- src/10-focus-input/FocusInput.js
+++ src/10-focus-input/FocusInput.js
@@ -1,8 +1,13 @@
+import { useRef, useEffect } from 'react';
+
 export default function FocusInput() {
+  const inputRef = useRef();
+  useEffect(() => inputRef.current.focus(), []);
+
   return (
     <div>
       <label htmlFor='focused-input'>Focus me on page load!</label>
-      <input name='focused-input'></input>
+      <input ref={inputRef} name='focused-input'></input>
     </div>
   )
 }
