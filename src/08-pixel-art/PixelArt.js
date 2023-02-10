bd516c9ad8c7bdd3a52c776f2057bb31f244296f
Heini Ahokas <heini.ahokas@klab-berlin.com>
2023-02-10 16:32:12 +0100

    implement color picker

--- src/08-pixel-art/PixelArt.js
+++ src/08-pixel-art/PixelArt.js
@@ -1,34 +1,21 @@
-import React from 'react'
+import { createContext, useMemo, useState } from 'react';
+import { ColorPicker, Pixels } from './';
 
-function ColorPicker () {
-  const colors = ['red', 'blue', 'yellow', 'green', 'black', 'white', 'purple']
+export const DrawingContext = createContext({
+  color: 'lightGrey',
+  setColor: undefined
+});
+
+export default function PixelArt() {
+  const [color, setColor] = useState('lightGrey');
+  const contextValue = useMemo(() => ({
+    color,
+    setColor
+  }), [color, setColor]);
   return (
-    <div>
-      <h1>Choose a color</h1>
-      {colors.map(color => <button key={color} style={{ backgroundColor: color }} />)}
-    </div>
-  )
-}
-
-function Pixel () {
-  return <div style={{ height: '20px', width: '20px', backgroundColor: 'lightGrey', margin: '1px' }} />
-}
-
-function Pixels () {
-  const pixels = []
-  for (let i = 0; i < 100; i++) pixels.push(<Pixel key={i} />)
-  return (
-    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(10, 1fr)', width: '210px', margin: '0 auto' }}>
-      {pixels}
-    </div>
-  )
-}
-
-export default function PixelArt () {
-  return (
-    <div>
+    <DrawingContext.Provider value={contextValue}>
       <ColorPicker />
       <Pixels />
-    </div>
+    </DrawingContext.Provider>
   )
 }
