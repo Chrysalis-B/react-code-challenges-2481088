08579f5a89e2094d9b8e268c923c245ee3ce1576
Heini Ahokas <heini.ahokas@klab-berlin.com>
2023-02-12 18:02:34 +0100

    Implement shopping cart

--- src/11-shopping-cart/ShoppingCart.js
+++ src/11-shopping-cart/ShoppingCart.js
@@ -1,18 +1,77 @@
-import { useState } from 'react'
+import { useReducer } from 'react';
+import { calculateTotal, getFormattedPrice, items } from './ShoppingCartHelpers';
 
-const items = [{
-  name: 'apple',
-  price: 0.39
-}, {
-  name: 'banana',
-  price: 0.79
-}, {
-  name: 'cherry tomatoes',
-  price: 3.99
-}]
+const initialState = {
+  cart: [],
+  total: 0
+}
 
-function ShoppingCart () {
-  const cart = [{ name: 'apple', quantity: 3, price: 0.39 }]
+function reducer(state, action) {
+  const { cart } = state;
+  if (action.type === 'ADD_TO_CART') {
+    if (cart.find(item => item.id === action.item.id)) {
+      const newCart = cart.map(item => {
+        if (item.id === action.item.id) {
+          return { ...item, quantity: item.quantity + 1 }
+        }
+        else {
+          return item;
+        }
+      });
+      const newTotal = calculateTotal(newCart);
+      return {
+        cart: newCart,
+        total: newTotal
+      }
+    }
+    else {
+      return {
+        cart: [
+          ...cart,
+          { ...action.item, quantity: 1 }
+        ],
+        total: calculateTotal([...cart, { ...action.item, quantity: 1 }])
+      }
+    }
+  }
+  if (action.type === 'DECREASE_QUANTITY') {
+    const newCart = cart.reduce((acc, item, index) => {
+      if (item.id === action.item.id) {
+        if (item.quantity === 1) {
+          return acc;
+        }
+        else {
+          acc[index] = { ...item, quantity: item.quantity - 1 };
+        }
+      }
+      else {
+        acc.push(item);
+      }
+      return acc;
+    }, []);
+    return {
+      cart: newCart,
+      total: calculateTotal(newCart)
+    }
+  }
+  if (action.type === 'INCREASE_QUANTITY') {
+    const newCart = cart.map(item => {
+      if (item.id === action.item.id) {
+        return { ...item, quantity: item.quantity + 1 }
+      }
+      else {
+        return item;
+      }
+    });
+    return {
+      cart: newCart,
+      total: calculateTotal(newCart)
+    }
+  }
+}
+
+function ShoppingCart() {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <div>
@@ -24,30 +83,30 @@ function ShoppingCart () {
             <div key={item.name}>
               <h3>{item.name}</h3>
               <p>${item.price}</p>
-              <button>Add to Cart</button>
+              <button onClick={() => dispatch({ type: 'ADD_TO_CART', item })}>Add to Cart</button>
             </div>)
           )}
         </div>
         <div>
           <h2>Cart</h2>
-          {cart.map(item => (
+          {state.cart.map(item => (
             <div key={item.name}>
               <h3>{item.name}</h3>
               <p>
-                <button>-</button>
+                <button onClick={() => dispatch({ type: 'DECREASE_QUANTITY', item })}>-</button>
                 {item.quantity}
-                <button>+</button>
+                <button onClick={() => dispatch({ type: 'INCREASE_QUANTITY', item })}>+</button>
               </p>
-              <p>Subtotal: ${item.quantity * item.price}</p>
+              <p>Subtotal: {getFormattedPrice(item.quantity * item.price)}</p>
             </div>
           ))}
         </div>
       </div>
       <div className='total'>
-        <h2>Total: $0.00</h2>
+        <h2>Total: {getFormattedPrice(state.total)}</h2>
       </div>
     </div>
   )
 }
 
-export default ShoppingCart
+export default ShoppingCart;
